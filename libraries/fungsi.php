b785ff7aa7ac53848567e8ede53e0fc2327db4a2
Muhammad Habib Taqiyullah <habibtaqiyullah9@students.unnes.ac.id>
2021-11-23 08:18:42 +0700

    menambah fungsi cek akses

--- libraries/fungsi.php
+++ libraries/fungsi.php
@@ -1,29 +1,40 @@
 <?php
-function save_data($koneksi,$nama_tabel,$data)
+function cek_akses($koneksi, $id_modul, $id_role, $action_modul)
 {
-    $sql = "INSERT INTO ".$nama_tabel." (";
+    $sql = "select * from modul_role where id_modul=$id_modul and id_role=$id_role and is_$action_modul=1 and deleted_at is null";
+    $data_modul_role = mysqli_query($koneksi, $sql);
+
+    if (mysqli_num_rows($data_modul_role) == 0) {
+        // tidak memiliki hak akses
+        return false;
+    }
+    return true;
+}
+
+function save_data($koneksi, $nama_tabel, $data)
+{
+    $sql = "INSERT INTO " . $nama_tabel . " (";
     $keys = array_keys($data);
     $values = array_values($data);
 
-    $sql .= implode(",",$keys).") ";
-    $sql .= "VALUES ('".implode("','",$values)."')";
+    $sql .= implode(",", $keys) . ") ";
+    $sql .= "VALUES ('" . implode("','", $values) . "')";
 
-    mysqli_query($koneksi,$sql);
+    mysqli_query($koneksi, $sql);
 }
 
-function update_data($koneksi,$nama_tabel,$data,$id,$primary_key)
+function update_data($koneksi, $nama_tabel, $data, $id, $primary_key)
 {
     // update [nama tabel] set col1=val1, ... where [primary_key]=[id]
     $sql = "UPDATE $nama_tabel SET ";
     $arr_update = [];
-    foreach($data as $k => $v)
-    {
-        $arr_update[]= $k."='".$v."'";
+    foreach ($data as $k => $v) {
+        $arr_update[] = $k . "='" . $v . "'";
     }
-    $sql .= implode(",",$arr_update);
-    $sql .= " WHERE $primary_key=".$id;
+    $sql .= implode(",", $arr_update);
+    $sql .= " WHERE $primary_key=" . $id;
 
-    mysqli_query($koneksi,$sql);
+    mysqli_query($koneksi, $sql);
 }
 
 function redirect($page)
